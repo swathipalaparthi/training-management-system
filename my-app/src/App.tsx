bd57fa0f3bf09afde98d8c3475de86100ba170fa
Swathi <swathi@divami.com>
2022-05-28 07:33:18 +0530

    Add Tab Component for admin

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import { TabComponent } from './components/tab-component/TabComponent';
+import { Admin } from './pages/admin/Admin';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+      <Route path="admin" element={ <Admin/> } />
+      {/* <Route path="login" element={ <Login/> } />
+      <Route path="sign-up" element={ <Signup/> } /> */}
+        {/* <Route path="/" element={ <UserDashboard/> } />
+        <Route path="course-details" element={ <CourseDetails/> } />
+        <Route path="courses" element={ <CourseCatrgories/> } /> */}
+      </Routes>
     </div>
   );
 }
---
+++ my-app/src/pages/admin/Admin.tsx
@@ -0,0 +1,23 @@
+import { TabComponent } from "../../components/tab-component/TabComponent"
+import { CourseTab } from "./CourseTab"
+import { EventsTab } from "./EventsTab"
+import { TopicsTab } from "./TopicsTab"
+
+export const Admin = () => {
+    const config = [{
+        component:CourseTab,
+        title:'Courses'
+    },
+    {
+        component:TopicsTab,
+        title:'Topics'
+    },
+    {
+        component:EventsTab,
+        title:'Events'
+    }
+]
+    return (<>
+    <TabComponent config={config}/>
+    </>)
+}
